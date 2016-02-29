0db149a0c3184e8cac5055aec4f929dd1787fdb5
raulett <raulett@gmail.com>
2016-03-01 06:34:53 +0800

    checkMove

--- src/main/java/game/Board.java
+++ src/main/java/game/Board.java
@@ -1,6 +1,7 @@
 package game;
 
 import game.figures.Figure;
+import game.figures.Knight;
 
 public class Board {
 
@@ -13,31 +14,95 @@ public class Board {
 
 	public Figure getCell(Coord coord) {
 
-		return cells[coord.getLetter().ordinal()][coord.getNum().ordinal()];
+		return cells[coord.getLetterOrd()][coord.getNumOrd()];
 	}
 	public Figure getCell(int i, int j) {
 		return cells[i][j];
 	}
 
 	/**
-	 * принимает на вход координаты  в виде пары значений перечислимого типа
-	 * ПЕРЕНЕС этот метод из фигуры сюда.
+	 * Метод отвечат на вопрос. Свободен ли путь фигуры из начальной точки в конечную?
+	 * Идея метода следующая.
+	 * 1) Проверяем по табличке, может ли фигура вообще пойти в искомую точку (первый иф)
+	 * 2) Проверяем препятствия на пути. Для этого надо понять, есть ли фигуры в клетках между начальной и конечной клетками маршрута. Начальную и конечную клетку не рассматриваем.
+	 * - Проверяем случай когда маршрут вертикальный
+	 * -горизонтальный
+	 * -диагональный
+	 * -это конь (маршрут пуст)
+	 *
 	 */
 	public boolean checkMove(Coord in, Coord out) {
 		if (getCell(in).getMoveTable(in, out) || getCell(in).getKillTable(in, out)){
-			int horizonDelta=0;
-			int verticalDelta=0;
+			int delta=0;
+			//Вертикальный ход фигуры
 			if (in.getLetter()==out.getLetter()){
-				verticalDelta = out.getNum().ordinal()-in.getNum().ordinal();
-				if (verticalDelta>0){
-					for (int i = in.getNum().ordinal()+1;i<out.getNum().ordinal();i++){
-						if (getCell(i, in.getLetter().ordinal()) != (null)){
+				delta = out.getNumOrd()-in.getNumOrd();
+				if (delta>0){
+					for (int i = in.getNumOrd()+1;i<out.getNumOrd();i++){
+						if (getCell(i, in.getLetterOrd()) != (null)){
 							return false;
 						}
 					}
-					//Дальше проверяем.
+					return true;
+				}
+				if (delta<0){
+					for (int i = in.getNumOrd()-1; i > out.getNumOrd(); i--){
+						if (getCell(i, in.getLetterOrd()) != (null)){
+							return false;
+						}
+					}
+					return true;
 				}
 			}
+			//Горизонтальный ход
+			if (in.getNum()==out.getNum()){
+				delta = out.getLetterOrd()-in.getLetterOrd();
+				if (delta>0) {
+					for (int i = in.getLetterOrd()+1; i < out.getLetterOrd(); i++){
+						if (getCell(i, in.getNumOrd()) != (null)){
+							return false;
+						}
+					}
+					return true;
+				}
+				if (delta<0) {
+					for (int i = in.getLetterOrd()-1; i > out.getLetterOrd(); i--){
+						if (getCell(i, in.getNumOrd()) != (null)){
+							return false;
+						}
+					}
+					return true;
+				}
+			}
+			//Диагональный ход
+			if (out.getLetterOrd()-in.getLetterOrd() == out.getNumOrd() - in.getNumOrd()){
+				delta = out.getLetterOrd()-in.getLetterOrd();
+				if (delta > 0){
+					int j = in.getNumOrd() + 1;
+					for (int i = in.getLetterOrd()+1; i < out.getLetterOrd(); i++){
+						if (getCell(i,j) != (null)){
+							return false;
+						}
+						j++;
+					}
+					return true;
+				}
+				if (delta < 0){
+					int j = in.getNumOrd() - 1;
+					for (int i = in.getLetterOrd() - 1; i > out.getLetterOrd(); i--){
+						if (getCell(i,j) != (null)){
+							return false;
+						}
+						j--;
+					}
+					return true;
+				}
+			}
+			//Проверяем, а не конь ли наша фигура
+			if (getCell(in) instanceof Knight){
+				return true;
+			}
+			return true;
 		}
 		return false;
 	}
