d1531ff53c20a3ab0de1be1e65c38b041112caf3
Dumgar <dumgar@yandex.ru>
2016-03-01 21:45:35 +0800

    add Player's check and mate

--- src/main/java/game/Player.java
+++ src/main/java/game/Player.java
@@ -3,6 +3,8 @@ package game;
 public class Player {
     String name;
     boolean color;
+    boolean check = false;
+    boolean mate = false;
 
     public Player(String name, boolean color) {
         this.name = name;
