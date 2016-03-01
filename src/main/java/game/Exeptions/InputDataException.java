2ffc48aa77853d32bed49db56d51305a10850d04
raulett <raulett@gmail.com>
2016-03-02 00:12:34 +0800

    Немножко поправил класс Фигура. Добавил конструктор, изменил матрицу

--- src/main/java/game/Exeptions/InputDataException.java
+++ src/main/java/game/Exeptions/InputDataException.java
@@ -4,5 +4,8 @@ package game.Exeptions;
  * Created by raulett on 01.03.2016.
  */
 public class InputDataException extends Throwable{
-
+    public InputDataException(String s){
+        new Throwable(s);
+    }
+    public InputDataException(){};
 }
